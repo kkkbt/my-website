ee90baf63c58ebf032025ccfb3c0d96441ed8b95
kkkbt <tokyo.tech1999soccer@gmail.com>
2021-08-21 20:23:21 +0900

    Update html and css

--- main.py
+++ main.py
@@ -15,6 +15,35 @@ def home():
     return render_template("index.html")
 
 
+@app.route("/application")
+def application():
+    return render_template("application.html")
+
+@app.route("/library")
+def library():
+    return render_template("library.html")
+
+
+@app.route("/biography")
+def biography():
+    return render_template("biography.html")
+
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
+
+
+@app.route("/investment")
+def investment():
+    return render_template("investment.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
 if __name__ == '__main__':
 
     app.run(debug=True)
